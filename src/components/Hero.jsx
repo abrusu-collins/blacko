611f5ea3d61740ca55ce8b10ab90656e459f7a38
abrusu-collins <snillochemingson@gmail.com>
2022-10-13 22:17:47 +0000

    NICE

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,20 @@
+
 function Hero() {
-    return ( 
-        <div className="hero">
-            
-        </div>
-     );
+  return (
+    <div className="hero">
+      <div className="left-flex">
+        <p className="name">Black Sherif <img src="" alt="" /></p>
+        <p className="about">
+          Black Sherif is the voice of the streets in Ghana right now. His music
+          speaks to the souls and hearts of his fans. He started his music
+          career at the age of 17 immediately after completing high school. He
+          had few songs doing well then but his succes story really started
+          after he released his mega hit titled "the first sermon".
+        </p>
+      </div>
+      <div className="right-flex"></div>
+    </div>
+  );
 }
 
 export default Hero;
