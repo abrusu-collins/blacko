d08a7648d48aca72cc75fff21871f766d15c04e4
abrusu-collins <snillochemingson@gmail.com>
2022-10-13 23:05:40 +0000

    bug fixing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,16 @@
-import musicicon from "../img/music.png";
 function Hero() {
   return (
     <div className="hero">
-        <p className="name">
-          Black Sherif <img src={musicicon} alt="musicicon" />
-        </p>
-        <div className="music-box"></div>
-        <p className="about">
-          Black Sherif is the voice of the streets in Ghana right now. His music
-          speaks to the souls and hearts of his fans. He started his music
-          career at the age of 17 immediately after completing high school. He
-          had few songs doing well then, but his success story really started
-          after he released his mega hit titled "the first sermon". After that,
-          everything he touches is a blessing. He is indeed a musical genius.
-        </p>
-        <a href=" ">Explore his music</a>
+      <div className="music-box"></div>
+      <p className="about">
+        Black Sherif is the voice of the streets in Ghana right now. His music
+        speaks to the souls and hearts of his fans. He started his music career
+        at the age of 17 immediately after completing high school. He had few
+        songs doing well then, but his success story really started after he
+        released his mega hit titled "the first sermon". After that, everything
+        he touches is a blessing. He is indeed a musical genius.
+      </p>
+      <a href=" ">Explore his music</a>
     </div>
   );
 }
