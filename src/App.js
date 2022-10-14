d10a8848c6f426deab843143e39295b42987a447
abrusu-collins <snillochemingson@gmail.com>
2022-10-14 13:39:58 +0000

    TVINW

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { AudioPlayerProvider } from "react-use-audio-player"
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import SocialNumbers from "./components/SocialNumbers";
+import TVINW from "./components/TVINW";
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
       <Hero file={"https://www.mboxdrive.com/Black-Sherif-Soja.mp3"}/>
         </AudioPlayerProvider>
         <SocialNumbers/>
+        <TVINW/>
     </>
   );
 }
--- src/components/TVINW.jsx
+++ src/components/TVINW.jsx
@@ -1,7 +1,11 @@
 function TVINW() {
     return ( 
         <div className="tvinw">
-            <h1></h1>
+            <p className="title"> DEBUT ALBUM</p>
+            <div className="album-name">
+                <p>THE VILLAIN I NEVER WAS</p>
+            </div>
+            <div className="listen"></div>
         </div>
      );
 }
