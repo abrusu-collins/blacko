c119df20187557783b892a842988cbbf278e5e33
abrusu-collins <snillochemingson@gmail.com>
2022-10-14 17:25:40 +0000

    comments added

--- src/components/Fans.jsx
+++ src/components/Fans.jsx
@@ -1,7 +1,67 @@
 function Fans() {
-    return ( 
-        <div className="fans"></div>
-     );
+  return (
+    <div className="fans">
+      <p className="title">THE FANS LOVE BLACKO</p>
+      <div className="comments">
+        <div className="comment1">
+          <p>Black Sherif is the definition of great talent </p>
+        </div>
+        <div className="comment2">
+          <p>
+            This guy is all style. Give him REGGAE, he will deliver. Give him
+            HIPLIFE he will deliver. Give him HIFLIFE, he will deliver and even
+            give him BLAKO and he will still deliver. His full soul is of
+            talents.
+          </p>
+        </div>
+        <div className="comment3">
+          <p>
+            Never expect disappointment from Black Sherif because even when he
+            was an underground,he never disappointed with his songs.Just accept
+            dat he was born for greatness
+          </p>
+        </div>
+        <div className="comment4">
+          <p>
+            This guy has perfected his art in a very excellent way. Black Sherif
+            is gonna be here for the longest time,when you listen to his songs
+            you can easily relate with what he's saying. Love from Kenya
+          </p>
+        </div>
+        <div className="comment5">
+          <p>
+            The energy, flow and voice are crazy, can't resist...black sherif is
+            gonna go places
+          </p>
+        </div>
+        <div className="comment6">
+          <p>
+            Wow!!!!! This is fire 🔥🔥🔥 As a white guy from Scotland 🏴󠁧󠁢󠁳󠁣󠁴󠁿 I
+            understood 5% of it 😂 but loved 100% of it
+          </p>
+        </div>
+        <div className="comment7">
+          <p>
+            I am in tears just watching this young dude. He is so good. I love
+            my West African brothers. 9ja
+          </p>
+        </div>
+        <div className="comment8">
+          <p>
+            Black seriff has something unique with his voice and the way he
+            jumps on the beat. Ghana to the world
+          </p>
+        </div>
+        <div className="comment9">
+          <p>
+            I'm from Texas and don't know how I ended up here ... but I'm glad I
+            did .. after hearing black sherif rap I had to go listen to him...
+            found a song called kwaku the traveler !! This dude is hard!!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Fans;
