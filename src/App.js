bd22703a02cdda0f5e1761cce01171aa5465dde9
abrusu-collins <snillochemingson@gmail.com>
2022-10-14 12:21:39 +0000

    styles coming

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./styles/index.css";
 import { AudioPlayerProvider } from "react-use-audio-player"
 
 
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import SocialNumbers from "./components/SocialNumbers";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
       <AudioPlayerProvider>
       <Hero file={"https://www.mboxdrive.com/Black-Sherif-Soja.mp3"}/>
         </AudioPlayerProvider>
+        <SocialNumbers/>
     </>
   );
 }
---
+++ src/components/SocialNumbers.jsx
@@ -0,0 +1,28 @@
+function SocialNumbers() {
+    return ( 
+        <div className="socialnumbers">
+            <div className="socialnumbersinner">
+                <div className="yt">
+                    <p className="title">YOUTUBE</p>
+                    <p className="count">640K+ Subscribers</p>
+                </div>
+                <div className="am">
+                    <p className="title">APPPLE MUSIC</p>
+                    <p className="count">640K+ Subscribers</p>
+                </div> <div className="ad">
+                    <p className="title">AUDIOMACK</p>
+                    <p className="count">1M+ followers</p>
+                </div> <div className="ig">
+                    <p className="title">INSTAGRAM</p>
+                    <p className="count">1.3M+ Followers</p>
+                </div>
+                <div className="tw">
+                    <p className="title">TWITTER</p>
+                    <p className="count">600K+ Folowers</p>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default SocialNumbers;
