5c892d971a29e924e66700e6caa4423f790d9816
abrusu-collins <snillochemingson@gmail.com>
2022-10-14 15:35:32 +0000

    I told you

--- src/components/TVINW.jsx
+++ src/components/TVINW.jsx
@@ -1,20 +1,22 @@
 import vid from "../img/vid.mp4";
-import rightarrow from "../img/right.png";
+import leftarrow from "../img/left.png";
+import downarrow from "../img/down.png";
+
 function TVINW() {
   return (
     <div className="tvinw">
       <p className="title"> DEBUT ALBUM</p>
       <div className="album-name">
-        <p>THE VILLAIN I NEVER WAS</p>
-
+        <div>
+          <p>THE VILLAIN I NEVER WAS</p>
+          <img src={downarrow} alt="" />
+        </div>
         <video id="background-video" autoPlay loop muted>
           <source src={vid} type="video/mp4" />
         </video>
       </div>
       <div className="listen">
-        <p>Listen to the album here</p>
-        <img src={rightarrow} alt="rightarrow" />
-       <iframe
+        <iframe
           title="mn"
           allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
           frameBorder="0"
@@ -28,6 +30,9 @@ function TVINW() {
           sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
           src="https://embed.music.apple.com/us/album/the-villain-i-never-was/1645609301"
         ></iframe>
+        <img src={leftarrow} alt="leftarrow" />
+
+        <p>Listen to the album here</p>
       </div>
     </div>
   );
