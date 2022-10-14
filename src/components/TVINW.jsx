975df50f450a2637303e2708b5fd9aeb17d3ed79
abrusu-collins <snillochemingson@gmail.com>
2022-10-14 13:53:25 +0000

    apple iframe added

--- src/components/TVINW.jsx
+++ src/components/TVINW.jsx
@@ -5,7 +5,12 @@ function TVINW() {
             <div className="album-name">
                 <p>THE VILLAIN I NEVER WAS</p>
             </div>
-            <div className="listen"></div>
+            <div className="listen">
+                <p>Listen to the album here</p>
+                <img src="" alt="" />
+                <iframe title="mn" allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameBorder="0" height="450" style={{width:"100%",maxWidth:"660px",overflow:"hidden", background:"transparent"}} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/the-villain-i-never-was/1645609301"></iframe>
+
+            </div>
         </div>
      );
 }
