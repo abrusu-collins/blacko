edc043c30fcd392cb57f30a4564d2f9dd5c42eef
abrusu-collins <snillochemingson@gmail.com>
2022-10-14 16:49:17 +0000

    nice

--- src/components/Stylish.jsx
+++ src/components/Stylish.jsx
@@ -1,12 +1,36 @@
+import _4 from "../img/4.jpg";
+import _8 from "../img/8.jpg";
+import _9 from "../img/9.jpg";
+import _10 from "../img/10.jpg";
+import _11 from "../img/11.jpg";
+import _12 from "../img/12.jpg";
+import _13 from "../img/13.jpg";
+import _18 from "../img/18.jpg";
+import _19 from "../img/19.jpg";
+import _24 from "../img/24.jpg";
+import _25 from "../img/25.jpg";
+import _26 from "../img/26.jpg";
+
 function Stylish() {
-    return ( 
-        <div className="stylish">
-            <p className="style">BLACKO GOT STYLE</p>
-            <div className="images">
-                
-            </div>
-        </div>
-     );
+  return (
+    <div className="stylish">
+      <p className="style">BLACKO GOT STYLE</p>
+      <div className="images">
+        <img src={_24} alt="" />
+        <img src={_25} alt="" />
+        <img src={_26} alt="" />
+        <img src={_4} alt="" />
+        <img src={_8} alt="" />
+        <img src={_9} alt="" />
+        <img src={_10} alt="" />
+        <img src={_11} alt="" />
+        <img src={_12} alt="" />
+        <img src={_13} alt="" />
+        <img src={_18} alt="" />
+        <img src={_19} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Stylish;
