58d5eef59a305baa4b582567439d4886f12cd118
abrusu-collins <snillochemingson@gmail.com>
2022-10-14 14:41:02 +0000

    Let's go

--- src/components/TVINW.jsx
+++ src/components/TVINW.jsx
@@ -1,16 +1,14 @@
 import vid from "../img/vid.mp4";
-import pic from "../img/TVINW.jpg";
 function TVINW() {
   return (
     <div className="tvinw">
       <p className="title"> DEBUT ALBUM</p>
       <div className="album-name">
-        {/* <p>THE VILLAIN I NEVER WAS</p> */}
+        <p>THE VILLAIN I NEVER WAS</p>
 
         <video id="background-video" autoPlay loop muted>
           <source src={vid} type="video/mp4" />
         </video>
-        <img src={pic} alt="tvinvcover" />
       </div>
       <div className="listen">
         <p>Listen to the album here</p>
