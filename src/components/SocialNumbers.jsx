9f23fe0fecd41cbd15985bbb8ba33a6986f139c3
abrusu-collins <snillochemingson@gmail.com>
2022-10-14 13:35:00 +0000

    errors checked

--- src/components/SocialNumbers.jsx
+++ src/components/SocialNumbers.jsx
@@ -1,28 +1,30 @@
 function SocialNumbers() {
-    return ( 
-        <div className="socialnumbers">
-            <div className="socialnumbersinner">
-                <div className="yt">
-                    <p className="title">YOUTUBE</p>
-                    <p className="count">640K+ Subscribers</p>
-                </div>
-                <div className="sp">
-                    <p className="title">SPOTIFY</p>
-                    <p className="count">1.3M+ monthly listeners</p>
-                </div> <div className="ad">
-                    <p className="title">AUDIOMACK</p>
-                    <p className="count">1M+ followers</p>
-                </div> <div className="ig">
-                    <p className="title">INSTAGRAM</p>
-                    <p className="count">1.3M+ Followers</p>
-                </div>
-                <div className="tw">
-                    <p className="title">TWITTER</p>
-                    <p className="count">600K+ Folowers</p>
-                </div>
-            </div>
+  return (
+    <div className="socialnumbers">
+      <div className="socialnumbersinner">
+        <div className="yt">
+          <p className="title">YOUTUBE</p>
+          <p className="count">640K+ Subscribers</p>
         </div>
-     );
+        <div className="sp">
+          <p className="title">SPOTIFY</p>
+          <p className="count">1.3M+ Monthly Listeners</p>
+        </div>{" "}
+        <div className="ad">
+          <p className="title">AUDIOMACK</p>
+          <p className="count">1M+ Followers</p>
+        </div>{" "}
+        <div className="ig">
+          <p className="title">INSTAGRAM</p>
+          <p className="count">1.3M+ Followers</p>
+        </div>
+        <div className="tw">
+          <p className="title">TWITTER</p>
+          <p className="count">600K+ Followers</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SocialNumbers;
---
+++ src/components/TVINW.jsx
@@ -0,0 +1,9 @@
+function TVINW() {
+    return ( 
+        <div className="tvinw">
+            <h1></h1>
+        </div>
+     );
+}
+
+export default TVINW;
