39d2ec1fd0b188727ea5b889d10d43a79a9acaed
abrusu-collins <snillochemingson@gmail.com>
2022-10-15 09:34:34 +0000

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
 function Footer() {
-    return ( 
-        <div className="footer"></div>
-     );
+  let year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      <p> FOLLOW BLACKO</p>
+      <div className="links">
+        <a href=" ">YouTube</a>
+        <a href=" ">Instagram</a>
+        <a href=" "> Twitter</a>
+      </div>
+      <div className="sedem">Developed by : DevTech &copy; {year}</div>
+    </div>
+  );
 }
 
 export default Footer;
