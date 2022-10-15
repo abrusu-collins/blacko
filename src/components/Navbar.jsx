d49cec6aa43df9b6dfc3290189a4095ad9f4f621
abrusu-collins <snillochemingson@gmail.com>
2022-10-15 22:23:39 +0000

    nice

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,6 +25,9 @@ function Navbar() {
           e.currentTarget.parentElement.children[1].classList.toggle(
             "navremoval"
           );
+          e.currentTarget.parentElement.classList.toggle(
+            "space"
+          );
         }}
       >
         <FontAwesomeIcon icon={faBars} />
