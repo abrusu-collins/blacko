dfd36bc8c0a6a285b083bc18bb091c5741d724cb
abrusu-collins <snillochemingson@gmail.com>
2022-10-26 10:10:23 +0000

    not the problem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles/index.css";
 import { AudioPlayerProvider } from "react-use-audio-player";
-import soja from "../src/mp3/soja.mp3";
+// import soja from "../src/mp3/soja.mp3";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -14,7 +14,7 @@ function App() {
     <>
       <Navbar />
       <AudioPlayerProvider>
-        <Hero file={soja} />
+        <Hero file={"https://www.mboxdrive.com/Black-Sherif-Soja.mp3"} />
       </AudioPlayerProvider>
       <SocialNumbers />
       <TVINW />
